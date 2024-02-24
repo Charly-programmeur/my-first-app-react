72aad412388a99c2e14575044871da1c01a30943
Charly-programmeur <charlynoudjalta.com>
2024-02-24 12:26:56 +0000

    react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  Button from 'react-bootstrap/Button';
+//import Container from 'react-bootstrap/Container';
+//import Navbar from 'react-bootstrap/Navbar';
+
+
+export default function App (){
+  return <div>
+    <h1>Test</h1>
+    <>
+      <title class ="App"/>
+      <button>Button test</button>
+    </>
+  </div>
+}
